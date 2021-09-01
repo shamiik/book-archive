1941ad673b12844dfec19fcc2df72fda0a3ff8c4
shamikdey <shamik7@gmail.com>
2021-09-02 05:50:45 +0600

    code update

--- main.js
+++ main.js
@@ -4,21 +4,30 @@ const searchBook = () => {
     // console.log(inputText);
 
     inputField.value = ' ';
+
+    const url = `https://openlibrary.org/search.json?q=${inputText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayBooks(data.docs))
 }
-const url = `https://openlibrary.org/search.json?q=chemistry`;
-fetch(url)
-    .then(res => res.json())
-    .then(data => displayBooks(data.docs))
+
 
 const displayBooks = books => {
-    // books.forEach(book => {
-    console.log(books);
-    // });
     const booksDiv = document.getElementById('books');
-    for (const book of books) {
-        // console.log(book);
-
-    }
-
+    books.forEach(book => {
+        console.log(book.title);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${book.title}</h5>
+                    <p class="card-text">This content is a little bit longer.</p>
+                </div>
+            </div>
+        `;
+        booksDiv.appendChild(div)
+    });
 
 }
