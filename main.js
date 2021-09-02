77274521439699f25dcf9e4b43362525a0dbcaf4
shamikdey <shamik7@gmail.com>
2021-09-02 22:30:59 +0600

    spinner fixed

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const errorDiv = document.getElementById('error');
 const booksContainer = document.getElementById('books');
 const bookFound = document.getElementById('found');
 const spinner = document.getElementById('spinner');
-// spinner.classList.add('d-none');
+
 //.......Search Button Function.......
 
 const searchBook = () => {
@@ -23,12 +23,15 @@ const searchBook = () => {
     //........Data Load.........
 
     const url = `https://openlibrary.org/search.json?q=${inputText}`;
+
     spinner.classList.remove('d-none');
     fetch(url)
         .then(res => res.json())
         .then(data => {
             displayBooks(data.docs);
+
             spinner.classList.add('d-none');
+
             //...... error handling .........
             if (data.numFound === 0) {
                 errorDiv.innerText = 'No Result Found!';
